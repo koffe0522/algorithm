f057edb7b0fc17856491b98307409aac1c3fe3d8
kouheikishigami <kouhei19920522@gmail.com>
2022-01-01 21:17:41 +0900

    edit cocktal sort

--- cocktail_sort.go
+++ cocktail_sort.go
@@ -7,13 +7,24 @@ func cocktailSort(numbers []int) []int {
 
 	for {
 		swapped = false
+
 		// ->
-		for i := range numbers[start:end] {
+		for i := start; i < end; i++ {
 			if numbers[i] > numbers[i+1] {
 				numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
 				swapped = true
 			}
 		}
+
+		/* Range Sintax
+		for i := range numbers[start:end] {
+			i = start + i
+			if numbers[i] > numbers[i+1] {
+				numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
+				swapped = true
+			}
+		}
+		*/
 
 		if !swapped {
 			break
@@ -22,18 +33,23 @@ func cocktailSort(numbers []int) []int {
 		end--
 
 		// reverse loop
-		// ->
-		for i := range numbers[start:end] {
-			i = end - i
-			if numbers[i-1] > numbers[i] {
-				numbers[i], numbers[i-1] = numbers[i-1], numbers[i]
+		// <-
+		for i := end; i > start-1; i-- {
+			if numbers[i] > numbers[i+1] {
+				numbers[i], numbers[i+1] = numbers[i+1], numbers[i]
 				swapped = true
 			}
 		}
 
-		if !swapped {
-			break
+		/* Range Sintax
+		for i := range numbers[start:end] {
+			j := end - i - 1
+			if numbers[j] > numbers[j+1] {
+				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
+				swapped = true
+			}
 		}
+		*/
 
 		start++
 	}
--- mian.go
+++ mian.go
@@ -5,7 +5,7 @@ import (
 )
 
 func main() {
-	numbers := []int{20, 64, 18, 4, 98, 80}
+	numbers := []int{20, 64, 18, 4, 98, 80, 110, 2, 8, 72, 12}
 	cocktailSort(numbers)
 	fmt.Println(numbers)
 }
